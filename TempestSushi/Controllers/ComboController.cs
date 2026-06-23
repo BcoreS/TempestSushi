ea03a2ae0b3012fd43a982edacb568411a740a4b
BcoreS <bcorestudy@gmail.com>
2026-06-22 21:58:48 -0600

    Razor view de Combo

---
+++ TempestSushi/Controllers/ComboController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TempestSushi.Application.Services.Interfaces;
+
+namespace TempestSushi.Controllers
+{
+    public class ComboController : Controller
+    {
+        private readonly IServiceCombo _serviceCombo;
+
+        public ComboController(IServiceCombo serviceCombo)
+        {
+            _serviceCombo = serviceCombo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Index()
+        {
+            if (TempData.ContainsKey("Mensaje"))
+            {
+                ViewBag.NotificationMessage = TempData["Mensaje"];
+            }
+
+            var collection = await _serviceCombo.ListAsync();
+            return View(collection);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int id)
+        {
+            var combo = await _serviceCombo.FindByIdAsync(id);
+
+            if (combo == null)
+            {
+                return NotFound();
+            }
+
+            return View(combo);
+        }
+    }
+}
