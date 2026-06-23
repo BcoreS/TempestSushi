cfc119362175a3caf261357c72e1b51006ffed8e
BcoreS <bcorestudy@gmail.com>
2026-06-22 20:00:48 -0600

    install automapper y crea services

---
+++ TempestSushi.Application/Services/Implementations/ServiceCombo.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TempestSushi.Application.Services.Implementations
+{
+    public class ServiceCombo
+    {
+    }
+}
